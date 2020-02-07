8d20234f53172a4b78d230ee1821855069daf2c2
Dylan Meivis <dylandiamonds@live.nl>
2020-02-07 23:54:15 +0100

    new touches

--- src/first-component/profile.js
+++ src/first-component/profile.js
@@ -11,6 +11,7 @@ class Profile extends React.Component {
         return (
         <div>
             <h1>This is the portfolio of {this.state.name}</h1>
+            <h3>Where you can find everything about me</h3>
             <button onClick={() => this.setState({name: 'Dylando'})}>Click me!</button>
         </div>
         );
