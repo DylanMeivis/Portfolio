7c39993f86923addddeaca5210c91713a58abaa6
Dylan Meivis <43239298+DylanMeivis@users.noreply.github.com>
2020-02-10 23:10:04 +0100

    Update test name

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders text on app page', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Hello and welcome/i);
   expect(linkElement).toBeInTheDocument();
