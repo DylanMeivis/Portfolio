f293c0f3cef7968515cd2af32f0e6479849944a5
Dylan Meivis <43239298+DylanMeivis@users.noreply.github.com>
2020-02-12 22:36:03 +0100

    remove input and changed to button

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  function handleThemeChange(e) {
-    if (e.target.checked) {
+  function handleThemeChange() {
       document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
   }
   return (
     <div className="mainhero">
         <div className="IntroductionImage">
             <h1>Hello 123</h1>
-            <div className="theme-switch-wrapper">
-                <label className="theme-switch" htmlFor="checkbox">
-                    <input type="checkbox" id="checkbox" onChange={handleThemeChange}/>
-                    <div className="slider round"></div>
-              </label>
-              <em>Enable Dark Mode!</em>
-            </div>
+            <button onClick={handleThemeChange}>Click for theme change </button>
         </div>
         <div className="InformationHeader">
             <h3>Dylan Meivis</h3>
