ee4821899d624b326be5794c23d3eec66b1aaa67
Dylan Meivis <43239298+DylanMeivis@users.noreply.github.com>
2020-02-12 21:31:05 +0100

    add function for changing theme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  function handleThemeChange(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  }
   return (
     <div className="mainhero">
         <div className="IntroductionImage">
             <h1>Hello 123</h1>
             <div className="theme-switch-wrapper">
                 <label className="theme-switch" htmlFor="checkbox">
-                    <input type="checkbox" id="checkbox" />
+                    <input type="checkbox" id="checkbox" onChange={handleThemeChange}/>
                     <div className="slider round"></div>
               </label>
               <em>Enable Dark Mode!</em>
