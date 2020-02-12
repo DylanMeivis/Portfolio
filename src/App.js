cf7e3487c39f373633d4a6cb179492c33dd540f1
Dylan Meivis <43239298+DylanMeivis@users.noreply.github.com>
2020-02-12 21:04:55 +0100

    New layout and button for theme switch

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello and welcome</h1>
-        <p>such portfolio</p>
-      </header>
+    <div class="mainhero">
+        <div class="IntroductionImage">
+            <h1>Hello 123</h1>
+            <div class="theme-switch-wrapper">
+                <label class="theme-switch" for="checkbox">
+                    <input type="checkbox" id="checkbox" />
+                    <div class="slider round"></div>
+              </label>
+              <em>Enable Dark Mode!</em>
+            </div>
+        </div>
+        <div class="InformationHeader">
+            <h3>Hello 123</h3>
+        </div>
     </div>
   );
 }
