51b075e3374221f56e207378ba92551455becbce
Dylan Meivis - AcceptEasy <dmeivis@accepteasy.com>
2020-02-19 21:14:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             <h3>Definitely yes</h3>
             <h3>Definitely no</h3>
             <h3>1</h3>
+            <h3>2</h3>
             <button>Okay bye now</button>
         </div>
     </div>
