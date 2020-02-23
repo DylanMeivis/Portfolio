37da9ef6a3ebb99c40cc441913cad4a1e76795a6
Dylan Meivis - AcceptEasy <dmeivis@accepteasy.com>
2020-02-23 23:24:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
             <h1>Nope</h1>
             <h1>Nope</h1>
             <h1>Nope</h1>
+            <h1>Nope</h1>
         </div>
         <div className="InformationHeader">
             <h3>Dylan Meivis </h3>
