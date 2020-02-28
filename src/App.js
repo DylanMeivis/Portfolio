8a8d9a7a899a23d0c65aa5f99b2369c8c8a9be8e
Dylan Meivis <43239298+DylanMeivis@users.noreply.github.com>
2020-02-28 23:45:23 +0100

    Update App.ja

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
             <h1>1</h1>
             <h1>2</h1>
             <h1>3</h1>
+            <h1>4</h1>
         </div>
         <div className="InformationHeader">
             <h3>Dylan Meivis </h3>
