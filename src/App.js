bd528f8ec9c7cfd412d1a6f5b6b208417cdc92a2
Dylan Meivis <43239298+DylanMeivis@users.noreply.github.com>
2020-02-29 23:14:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
             <h1>Jupppss</h1>
             <h1>1</h1>
             <h1>2</h1>
-            <h1>3</h1>
             <h1>4</h1>
         </div>
         <div className="InformationHeader">
