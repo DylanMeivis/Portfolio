bf438b454061085015b4100ac193bf9ed2f5474a
Dylan Meivis - AcceptEasy <dmeivis@accepteasy.com>
2020-03-11 22:31:26 +0100

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -6,39 +6,12 @@ function App() {
       document.documentElement.setAttribute('data-theme', 'dark');
   }
   return (
-    <div className="mainhero">
+    <div className="mainhero container-fluid p-0">
         <div className="IntroductionImage">
-            <h1>Hello 123</h1>
-            <h1>Is it me your looking for?</h1>
-            <button onClick={handleThemeChange}>Enable Dark Mode!</button>
-            <h3>Dork mode1</h3>
-            <h3>Such Dork2</h3>
-            <h1>Does this fit?</h1>
-            <h1>This definitely does not fit</h1>
-            <h1>Nope</h1>
-            <h1>Nope</h1>
-            <h1>Nope</h1>
-            <h1>Nope</h1>
-            <h1>Nope</h1>
+          <h3>Dylan Meivis</h3>
+            <button className="btn btn-primary" onClick={handleThemeChange}>Enable Dark Mode!</button>
         </div>
         <div className="InformationHeader">
-            <h3>Dylan Meivis </h3>
-            <h3>Portfolio</h3>
-            <h3>Developer</h3>
-            <h3>And this?</h3>
-            <h3>Definitely yes</h3>
-            <h3>Definitely no</h3>
-            <h3>1</h3>
-            <h3>2</h3>
-            <h3>3</h3>
-            <h3>4</h3>
-            <h3>7</h3>
-            <button>Okay bye now</button>
-            <h3>Yup</h3>
-            <h3>Nope</h3>
-            <h3>Yup</h3>
-            <h3>Nope</h3>
-            <h3>Yes</h3>
         </div>
     </div>
   );
