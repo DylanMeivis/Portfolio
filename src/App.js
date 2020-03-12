c8a7a325e5fcebc54cc9a146feb113f9d73a51c5
Dylan Meivis - AcceptEasy <dmeivis@accepteasy.com>
2020-03-12 23:31:31 +0100

    Introduce Profile picture 📷

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <div className="row">
+      <div className="row pt-2">
+        <div className="col-12 text-center">
+          <img src="http://placekitten.com/200/300" className="rounded" alt="profilePicture"></img>
+        </div>
         <div className="col-12 text-center">
           <h1>Dylan Meivis</h1>
-          <h4>🥑 Full-Stack Developer <a href="https://www.accepteasy.com">@AcceptEasy</a></h4>
+          <h5>Full-Stack Developer <a href="https://www.accepteasy.com">@AcceptEasy</a></h5>
         </div>
       </div>
     </div>
