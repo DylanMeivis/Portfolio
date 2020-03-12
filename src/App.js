df3c271c74906dca57895d6edbd9cab1845c894e
Dylan Meivis - AcceptEasy <dmeivis@accepteasy.com>
2020-03-12 23:33:23 +0100

    Change quotes ✏

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row pt-2">
-        <div className="col-12 text-center">
-          <img src="http://placekitten.com/200/300" className="rounded" alt="profilePicture"></img>
+    <div className='container'>
+      <div className='row pt-2'>
+        <div className='col-12 text-center'>
+          <img src='http://placekitten.com/200/300' className='rounded' alt='profilePicture'></img>
         </div>
-        <div className="col-12 text-center">
+        <div className='col-12 text-center'>
           <h1>Dylan Meivis</h1>
-          <h5>Full-Stack Developer <a href="https://www.accepteasy.com">@AcceptEasy</a></h5>
+          <h5>Full-Stack Developer <a href='https://www.accepteasy.com'>@AcceptEasy</a></h5>
         </div>
       </div>
     </div>
