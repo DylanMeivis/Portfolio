7b64eb7f1ad8c46bc9234f0ec7d5146aabae2fb1
Dylan Meivis - AcceptEasy <dmeivis@accepteasy.com>
2020-03-13 23:33:28 +0100

    Update navbar links 🎆

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ function App() {
   return (
     <div className='container-fluid p-0'>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="https://dylanmeivis.github.io/portfolio">DM</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Projects</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link" href="#">Contact</a>
             </li>
           </ul>
         </div>
